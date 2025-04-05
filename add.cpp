628d12e2cea0db14f060928bfcda0f7448c5f183
Devanshu07R <devanshudasgupta142004@gmail.com>
2025-04-05 22:05:59 +0530

    Initial commit - C-Programming-language

---
+++ add.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+    
+int main(){
+
+    int a,b,add;
+    cout<<"Enter a:";
+    cin>>a;
+
+    cout<<"Enter b:";
+    cin>>b;
+
+    add=a+b;
+
+    cout<<"add of a and b is:"<<add;
+
+     return 0;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"HEllO WORLD";
+    return 0;
+
+
+}
+
+
+
